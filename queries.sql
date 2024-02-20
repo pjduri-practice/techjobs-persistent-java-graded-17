2e02d3f0678aa2b6f226b7a027174e05210e5251
Your Name <108307350+pjduri@users.noreply.github.com>
2024-02-20 14:39:34 -0600

    task 4, all tests pass

--- queries.sql
+++ queries.sql
@@ -4,16 +4,16 @@
 --
 --DESCRIBE job;
 --Part 2
---SELECT name
---FROM employers
---WHERE location = "St Louis City";
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 --Part 3
--- DROP TABLE job;
+ DROP TABLE job;
 
 --Part 4
---SELECT *
---FROM skill
---INNER JOIN job_skills
---	ON skill.id = job_skills.skills_id
---WHERE job_skills.jobs_id IS NOT NULL
---ORDER BY name ASC;
+SELECT *
+FROM skill
+INNER JOIN job_skills
+	ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
